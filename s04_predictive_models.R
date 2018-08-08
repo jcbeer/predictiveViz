1e96dc5118fad6ce8ddc03fdb9652a1651b7775a
Joanne Beer <joannecbeer@gmail.com>
2018-08-08 13:53:50 -0700

    adding data

--- s04_predictive_models.R
+++ s04_predictive_models.R
@@ -19,7 +19,7 @@ test_n <- ceiling(test_proportion*dim(df)[1])
 test_n
 # randomly select test set
 # set random seed
-set.seed(1)
+set.seed(3)
 # divide indicies into training and test
 total_n <- dim(df)[1]
 test_ids <- sample(1:total_n, test_n)
@@ -47,6 +47,7 @@ X_test_std <- scale(X_test, center=X_train_mean, scale=X_train_sd)
 
 # Elastic net
 library(glmnet)
+# SET UP TUNING PARAMETER SEQUENCE
 alpha_sequence_min <- 0.2
 alpha_sequence_max <- 1
 alpha_sequence_length <- 6
@@ -58,48 +59,49 @@ lambda <- rev(exp(seq(lambda_sequence_min, lambda_sequence_max, length.out=lambd
 # save the sequences of alphas and lambdas in order
 alpha_sequence <- rep(alpha, each=lambda_sequence_length)
 lambda_sequence <- rep(lambda, alpha_sequence_length)
-# save estimated coefficients and predicted y
+# SAVE ESTIMATED COEFFICIENTS
 # for 120 tuning parameter combinations
-# first row is alpha squence
-# second row is lambda sequence
-# first column of predicted_y is actual y
-coefs <- matrix(nrow=(dim(df)[2] + 2), ncol=alpha_sequence_length*lambda_sequence_length)
-coefs[1,] <- alpha_sequence
-coefs[2,] <- lambda_sequence
-predicted_y <- matrix(nrow=(length(y_test) + 2), ncol=(alpha_sequence_length*lambda_sequence_length + 1))
-predicted_y[1,2:dim(predicted_y)[2]] <- alpha_sequence
-predicted_y[2,2:dim(predicted_y)[2]] <- lambda_sequence
-predicted_y[3:dim(predicted_y)[1], 1] <- y_test
-# we want to add some metrics to the predicted y
-# pearson correlation
-# MSE
-# MAE
-# regression coefficients ..?
-# or maybe Nate's code can do it automatically...
-# put numeric placeholders for missing values
-predicted_y[1,1] <- 9999
-predicted_y[2,1] <- 9999
+coefs <- matrix(nrow=(dim(df)[2] - 2), ncol=alpha_sequence_length*lambda_sequence_length)
 # loop over the alpha values and store data
-for (a in 1:length(alpha)){
-  fit <- glmnet(X_train_std, y_train_centered, standardize=FALSE, intercept=FALSE, lambda=lambda, family='gaussian', alpha=a)
-  
+for (i in 1:length(alpha)){
+  fit <- glmnet(X_train_std, y_train_centered, standardize=FALSE, intercept=FALSE, lambda=lambda, family='gaussian', alpha=alpha[i])
+  # store coefficients
+  coef_first_index <- i*lambda_sequence_length - 19
+  coef_last_index <- i*lambda_sequence_length
+  coefs[, coef_first_index:coef_last_index] <- as.matrix(coef(fit))[-1,]
 }
+# CALCULATE PREDICTED Y
+# predict the centered y
+predy <- X_test_std %*% coefs
+# add back the mean
+predy_adj <- predy + y_train_mean
+plot(y_test, predy_adj[,70])
+# first column of predicted_y is actual y
+predicted_y <- cbind(y_test, predy_adj)
 
+# SAVE STATISTICS
+# we want to add some metrics to the predicted y
+# columns in order:
+# 1. alpha squence
+# 2. lambda sequence
+# 3. pearson correlation
+# 4. p-value
+# 5. beta0 -- intercept
+# 6. beta1 -- slope
+# 7. RMSE
+# 8. MAE
+stats <- matrix(ncol=8, nrow=alpha_sequence_length*lambda_sequence_length)
+stats[,1] <- alpha_sequence
+stats[,2] <- lambda_sequence
+stats[,3] <- apply(predicted_y[,2:dim(predicted_y)[2]], 2, function(x) cor.test(x, predicted_y[,1])$estimate)
+stats[,4] <- apply(predicted_y[,2:dim(predicted_y)[2]], 2, function(x) cor.test(x, predicted_y[,1])$p.value)
+stats[,5] <- apply(predicted_y[,2:dim(predicted_y)[2]], 2, function(x) coef(lm(x ~ predicted_y[,1]))[1])
+stats[,6] <- apply(predicted_y[,2:dim(predicted_y)[2]], 2, function(x) coef(lm(x ~ predicted_y[,1]))[2])
+stats[,7] <- apply(predicted_y[,2:dim(predicted_y)[2]], 2, function(x) sqrt(mean((x - predicted_y[,1])^2)))
+stats[,8] <- apply(predicted_y[,2:dim(predicted_y)[2]], 2, function(x) mean(abs((x - predicted_y[,1]))))
 
+# SAVE DATA
+write.table(coefs, '/Users/Psyche/neurohack2018/predictiveViz/data/coefficients.csv', row.names = FALSE, col.names = FALSE, sep=',', na='')
+write.table(predicted_y, '/Users/Psyche/neurohack2018/predictiveViz/data/predicted_y.csv', row.names = FALSE, col.names = FALSE, sep=',', na='')
+write.table(stats, '/Users/Psyche/neurohack2018/predictiveViz/data/stats.csv', row.names = FALSE, col.names = FALSE, sep=',', na='')
 
-
-regr = ElasticNet(random_state=1)
-regr.fit(X_train_std, y_train_centered)
-ElasticNet(alpha=1, copy_X=True, fit_intercept=False, l1_ratio=0.25,
-           max_iter=10000, normalize=True, positive=False, precompute=False,
-           random_state=1, selection='random', tol=0.0001, warm_start=False)
-
-print(regr.coef_) 
-print(regr.intercept_) 
-print(regr.predict(X_test)) 
-
-# calculate prediction error
-pred_y_test = regr.predict(X_test)
-print(y_test)
-print(pred_y_test + y_train_mean)
-np.subtract(pred_y_test, y_test_centered)
